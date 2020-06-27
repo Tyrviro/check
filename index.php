a9b57d88d5d11a0f1fde9aa90a8526c5e2fb8529
Tyrviro <corentin.bozec@epitech.eu>
2020-06-27 12:47:32 +0200

    test

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <html>
     <title>Check</title>
-    <meta name ="salut" content="test.php" />
     <body>
         thanks to wait
-        <?php
-        if ($_POST["prenom"] == "Yannick") {
-            header('Location: https://hindou.netlify.app/');
-        }
-        else {
-            header('Location: https://welcom.netlify.app/');
-        }
-        ?>
     </body>
 </html>
